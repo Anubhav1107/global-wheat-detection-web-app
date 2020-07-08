7064a23ed7096656f88999891a98b3bdd726a562
Memphis Meng <1310750076@qq.com>
2020-07-08 16:46:07 -0400

    modify text

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     outputs = None
     images = None
 
-    uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+    uploaded_file = st.file_uploader("Choose an image... (jpg only)", type="jpg")
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, caption='Uploaded Image', use_column_width=True)
